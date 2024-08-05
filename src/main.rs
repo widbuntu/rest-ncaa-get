96f50e1ee5317b8e94d063dbf291ba76e9a46022
widbuntu <widbuntu@gmail.com>
2024-08-05 15:24:11 -0600

    commit

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,28 @@ use actix_web::{web, App, HttpServer};
 use actix_cors::Cors;
 use std::sync::Arc;
 use crate::repository::ddb::{SSHRepository, get_team_history, get_teams, get_team_season, get_current_teams_view, get_current_season};
+use dotenv::dotenv;
+use std::env;
 
 mod repository {
     pub mod ddb;
 }
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let ssh_host = env::var("SSH_HOST").expect("SSH_HOST not set");
+    let ssh_port: u16 = env::var("SSH_PORT").expect("SSH_PORT not set").parse().expect("Invalid SSH_PORT");
+    let ssh_user = env::var("SSH_USER").expect("SSH_USER not set");
+    let ssh_password = env::var("SSH_PASSWORD").expect("SSH_PASSWORD not set");
+
     let repo = Arc::new(SSHRepository::new(
-        String::from("192.168.1.252"),
-        22,
-        String::from("widbuntu"),
-        String::from("Office366!")
+        ssh_host,
+        ssh_port,
+        ssh_user,
+        ssh_password
     ));
 
     HttpServer::new(move || {
@@ -26,7 +36,7 @@ async fn main() -> std::io::Result<()> {
             .route("/team-season", web::get().to(get_team_season))
             .route("/current-season", web::get().to(get_current_season))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
